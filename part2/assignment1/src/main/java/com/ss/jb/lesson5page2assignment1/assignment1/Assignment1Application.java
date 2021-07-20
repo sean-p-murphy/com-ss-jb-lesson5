4ae8332b5353568331e598cd7adcb1c2bf421642
murphcoder <murphcoder@gmail.com>
2021-07-20 18:42:34 -0500

    Part 2 Assignment 1 Corrected

--- part2/assignment1/src/main/java/com/ss/jb/lesson5page2assignment1/assignment1/Assignment1Application.java
+++ part2/assignment1/src/main/java/com/ss/jb/lesson5page2assignment1/assignment1/Assignment1Application.java
@@ -2,52 +2,55 @@ package com.ss.jb.lesson5page2assignment1.assignment1;
 
 public class Assignment1Application {
 
-	public void test(String info) {
+	public static interface NumberTester {
+		public String test(Integer num);
+	}
+
+	public void tester(String info) {
+
+		NumberTester isOdd = (num) -> (num % 2 == 0) ? "EVEN" : "ODD";
+
+		NumberTester isPrime = (num) -> {
+			boolean flag = true;
+			for (int i = 2; i <= num / 2; i++) {
+				if (num % i == 0) {
+					flag = false;
+					break;
+				}
+			};
+			return (flag) ? "PRIME" : "COMPOSITE";
+		};
+
+		NumberTester isPalindrome = (num) -> {
+			StringBuilder forward = new StringBuilder(String.valueOf(num));
+			return (forward == forward.reverse()) ? "PALINDROME" : "NOT A PALINDROME";
+		};
+
 		String[] arr = info.split(" ");
 		if (arr.length == 2) {
 			switch (Integer.parseInt(arr[0])) {
 				case 1:
-				System.out.println(isOdd(Integer.parseInt(arr[1])));
+				System.out.println(isOdd.test(Integer.parseInt(arr[1])));
 				break;
 				case 2:
-				System.out.println(isPrime(Integer.parseInt(arr[1])));
+				System.out.println(isPrime.test(Integer.parseInt(arr[1])));
 				break;
 				case 3:
-				System.out.println(isPalindrome(Integer.parseInt(arr[1])));
+				System.out.println(isPalindrome.test(Integer.parseInt(arr[1])));
 				break;
 			}
 		}
 	}
 
-	public String isOdd(int num) {
-		return (num % 2 == 0) ? "EVEN" : "ODD";
-	};
-
-	public String isPrime(int num) {
-		boolean flag = true;
-		for (int i = 2; i <= num / 2; i++) {
-			if (num % i == 0) {
-				flag = false;
-				break;
-			}
-		};
-		return (flag) ? "PRIME" : "COMPOSITE";
-	};
-
-	public String isPalindrome(int num) {
-		StringBuilder forward = new StringBuilder(String.valueOf(num));
-		return (forward == forward.reverse()) ? "PALINDROME" : "NOT A PALINDROME";
-	}
-
 	public static void main(String[] args) {
 
 		Assignment1Application tester = new Assignment1Application();
-		tester.test("5");
-		tester.test("1 4");
-		tester.test("2 5");
-		tester.test("3 898");
-		tester.test("1 3");
-		tester.test("2 12");
+		tester.tester("5");
+		tester.tester("1 4");
+		tester.tester("2 5");
+		tester.tester("3 898");
+		tester.tester("1 3");
+		tester.tester("2 12");
 	}
 
 }
